8992d2799a27721cb91c423f0009e0870af0e3dd
Hlushkova Maria <marusia2200@gmail.com>
2022-05-10 21:55:44 +0300

    10.05.2022 21:55

--- FoodApp/FoodApp/AddPage.xaml.cs
+++ FoodApp/FoodApp/AddPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,30 @@ namespace FoodApp
     /// </summary>
     public partial class AddPage : Page
     {
+        DbRecipes db; //Для работы с базой
         public AddPage()
         {
             InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            db = new DbRecipes();
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = "c:\\";
+            dlg.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string selectedFileName = dlg.FileName;
+                FileNameLabel.Content = selectedFileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedFileName);
+                bitmap.EndInit();
+                ImageViewer1.Source = bitmap;
+            }
         }
     }
 }
